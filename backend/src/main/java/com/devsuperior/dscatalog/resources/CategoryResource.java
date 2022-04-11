5a2de3ea475c14d5f6952b9144bad75ca4da2e92
westorres9 <wester.torres9@gmail.com>
2022-04-11 18:50:49 -0300

    implemented CategoryResource

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dscatalog.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscatalog.DTO.CategoryDTO;
+import com.devsuperior.dscatalog.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
+		Page<CategoryDTO> page = service.findAll(pageable);
+		return ResponseEntity.ok().body(page);
+	}
+}
