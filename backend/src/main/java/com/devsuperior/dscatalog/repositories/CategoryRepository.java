3275cc8ce557663911852cda2e2a8de9baa03995
westorres9 <wester.torres9@gmail.com>
2022-04-11 18:34:31 -0300

    implemented CategoryDTO

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dscatalog;
+package com.devsuperior.dscatalog.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
