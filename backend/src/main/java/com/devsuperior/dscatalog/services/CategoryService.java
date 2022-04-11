83396ee43c1b9c1b624dc1e12d13137021fe483c
westorres9 <wester.torres9@gmail.com>
2022-04-11 20:19:13 -0300

    implements ResourceNotFoundException in Service Layer

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.DTO.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -28,7 +29,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 		public CategoryDTO findById(Long id) {
 			Optional<Category> obj = repository.findById(id);
-			Category entity = obj.get();
+			Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Resource Not Found"));
 			return new CategoryDTO(entity);
 		}
 	
