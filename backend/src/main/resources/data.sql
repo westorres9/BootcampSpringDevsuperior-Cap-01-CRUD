5c99c4f4f2164b233115c11f62b15f9dd6e8986f
westorres9 <wester.torres9@gmail.com>
2022-04-11 22:32:13 -0300

    created entity of Products

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,5 @@
-INSERT INTO tb_category (name) VALUES ('Livros');
-INSERT INTO tb_category (name) VALUES ('Computadores');
-INSERT INTO tb_category (name) VALUES ('Eletronicos');
+INSERT INTO tb_category (name, created_At) VALUES ('Livros', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Computadores', NOW());
+INSERT INTO tb_category (name, created_At) VALUES ('Eletronicos', NOW());
+
+
